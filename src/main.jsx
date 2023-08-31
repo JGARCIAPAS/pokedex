055c708bee7d543f45d36e801046f50ae357bdc7
Jorge <jorge.garcia.pastor@gmail.com>
2023-08-31 12:05:19 +0200

    ADD added styles to main screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Pokedex from './Pokedex'
+import './assets/styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Pokedex />
   </React.StrictMode>,
 )
